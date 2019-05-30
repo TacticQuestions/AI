e2ab8ce7573f96cf8cf274eb93e3221f01b5448b
Taha Ali <karatemastertaha@gmail.com>
2019-05-30 15:37:00 +0500

    Base Window Added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
 #include <windows.h>
+
+LRESULT CALLBACK WindowProcidure(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor =  LoadCursor(NULL,IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"AIClass";
+    wc.lpfnWndProc = WindowProcidure;
+
+    if(!RegisterClassW(&wc))
+    {
+        return -1;
+    }
+
+    CreateWindowW(L"AIClass", L"AI", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 200,200,500,500,NULL,NULL,NULL,NULL);
+    MSG msg = {0};
+    while( GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WindowProcidure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch ( msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hWnd, msg, wp, lp);
+    }
     return 0;
 }
